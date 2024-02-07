ba3620a659d56fe63b0fa94f57cd00a6b0ed5eb1
Mihail-2020 <mihailorenikov06440@gmail.com>
2024-02-07 23:20:33 +0300

    save commit end day

--- app.js
+++ app.js
@@ -1,4 +1,57 @@
+const shipsTableElement = document.querySelector('#tbody');
+
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  return response.json();
+}
+
+
+// const SHIPS_PAGES = 4;
+// const SHIPS_URL = 'https://swapi.dev/api/starships/?page=';
+
+// async function fetchAllShips() {
+// 	shipsTableElement.innerHTML = ''
+
+//   for (let i = 1; i < SHIPS_PAGES; i++) {
+//     let url = SHIPS_URL + i;
+
+//     try {
+//       const data = await fetchData(url);
+//       const array = data.results;
+
+//       for (let item in array) {
+//         const tr = document.createElement("tr");
+//         tr.innerHTML = `
+//         <td>${array[item].name}</td>
+//         <td>${array[item].model}</td>
+//         <td>${array[item].starship_class}</td>
+//         <td>${array[item].manufacturer}</td>
+//       `;
+//         shipsTableElement.appendChild(tr);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+// }
+
+// fetchAllShips();
+// updateShipsButtonElement.addEventListener('click', fetchAllShips);
+
+
+
+
+
+
+
+
+
+
+
 function renderSection() {
+
+
 const sectionElement = `
     <div class="table-block">
     <table>
--- main.js
+++ main.js
@@ -1,39 +1,12 @@
-const shipsTableElement = document.querySelector('#tbody');
 
+const sectionStarWars = document.querySelector('#star-wars');
 
-async function fetchData(url) {
-  const response = await fetch(url);
-  return response.json();
+sectionStarWars.addEventListener('click', handelSectionClick);
+
+function handelSectionClick(event) {
+  let section = event.target.id;
+
+  console.log(section);
 }
 
-const SHIPS_PAGES = 4;
-const SHIPS_URL = 'https://swapi.dev/api/starships/?page=';
 
-async function fetchAllShips() {
-	shipsTableElement.innerHTML = ''
-
-  for (let i = 1; i < SHIPS_PAGES; i++) {
-    let url = SHIPS_URL + i;
-
-    try {
-      const data = await fetchData(url);
-      const array = data.results;
-
-      for (let item in array) {
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
-        <td>${array[item].name}</td>
-        <td>${array[item].model}</td>
-        <td>${array[item].starship_class}</td>
-        <td>${array[item].manufacturer}</td>
-      `;
-        shipsTableElement.appendChild(tr);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-}
-
-fetchAllShips();
-updateShipsButtonElement.addEventListener('click', fetchAllShips);
